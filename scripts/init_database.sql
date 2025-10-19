da9830b0494afcaf08bbe20a9de1d333cfd1ae5f
Abdullah Rufai <abdullahrufai72@gmail.com>
2025-10-19 20:59:24 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,14 @@
+/*
+*******************************************************
+Create Database and Schema
+*******************************************************
 
+Script Purpose:
+This script creates a new database named 'DataWarehouse'. 
+If it already exists, it is dropped and recreated. 
+Additionally, the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+Running this script will drop the entire 'DataWarehouse' database permanently if it exists.
+Proceed with caution and ensure you have a proper backup before running this script.
+*/
